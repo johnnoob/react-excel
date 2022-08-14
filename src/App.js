a19376d73214c4df067e20339bf403c29cdba906
s079053002006 <s079053002006@gmail.com>
2022-08-15 07:25:32 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from 'react'
+import * as XLSX from 'xlsx'
 
 function App() {
+  const [file, setFile] = useState(null)
+  const [fileName, setFileName] = useState(null)
+  const fileRef = useRef('')
+  const handleFile = async (e) => {
+    const file = e.target.files[0]
+    if (!file) return
+
+    //Read excel
+    const data = await file.arrayBuffer()
+    const wb = XLSX.read(data)
+    console.log(wb)
+
+    setFile(file)
+    setFileName(file.name)
+  }
+  const removeFile = () => {
+    fileRef.current.value = ''
+    setFile(null)
+    setFileName(null)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <h2 className="mb-3">上傳檔案</h2>
+      <p className="text-muted mb-0">{fileName ? fileName : '請上傳檔案'}</p>
+      <div className="row">
+        <div className="col">
+          <div className="input-group mb-3">
+            <label className="input-group-text" htmlFor="inputGroupFile01">
+              上傳excel
+            </label>
+            <input
+              ref={fileRef}
+              type="file"
+              className="form-control"
+              id="inputGroupFile01"
+              onChange={(e) => handleFile(e)}
+            />
+          </div>
+          <button className="btn btn-danger" onClick={removeFile}>
+            刪除
+          </button>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
